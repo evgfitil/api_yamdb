adb507545e47097436eb1076254fb866a74459a1
Евгений Ахметзянов <eugenueakhmetzyanov@evgakhm-mbp.local>
2020-07-24 16:06:57 +0500

    Change database settings

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -78,10 +78,6 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
     'ENGINE': os.getenv('SQL_ENGINE', 'django.db.backends.sqlite3'),
     'NAME': os.getenv('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
