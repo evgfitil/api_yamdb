64f988a39ce374e385df91a8831aa96826a5b46a
Евгений Ахметзянов <eugenueakhmetzyanov@evgakhm-mbp.local>
2020-10-04 23:42:09 +0300

    Disable debug

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -23,7 +23,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env.dev'))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
