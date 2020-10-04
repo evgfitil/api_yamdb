86ad288b035c366ee5770eeeb8aa38e6331710e3
Евгений Ахметзянов <eugenueakhmetzyanov@evgakhm-mbp.local>
2020-10-04 23:59:40 +0300

    Update settings

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,8 @@ urlpatterns = [
          TemplateView.as_view(template_name='redoc.html'),
          name='redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
